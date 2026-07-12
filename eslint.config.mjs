56e151dc49b451d381ff69238673dbb26ee20a5e
Martina Papp <martinapapp0@gmail.com>
2026-07-12 15:18:04 +0200

    fix format issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,9 @@ export default defineConfig([
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
     rules: {
-      "no-console": "warn",
-      "prefer-const": "error"
-    }
+      'no-console': 'warn',
+      'prefer-const': 'error',
+    },
   },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
 ]);
